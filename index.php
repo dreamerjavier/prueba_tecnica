74260c378d9bf10c6d039e91d9297de4a665eb11
Javier <javierguerrero@balabox.com>
2021-06-04 15:48:16 +0200

    recursive algotrythm

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ class Globals
 
 class Relacion extends Globals
 {
+    public $inHouseRepos = [];
+    public $composerList = [];
+
     public function getPackageRelation($packageName = null)
     {
         /* if (is_null($packageName))
@@ -22,8 +25,10 @@ class Relacion extends Globals
     public function createTree(string $directory = null)
     {
         $arrayResponse = [];
-        $composerList = [];
         $flatElements = [];
+
+        $descendantTree = [];
+        $ascendantTree = [];
 
         // Lectura de repositorios
         $globals = new Globals;
@@ -33,41 +38,48 @@ class Relacion extends Globals
             if (!empty($scanResult)) {
                 foreach ($scanResult as $result) {
                     if (!pathinfo($result, PATHINFO_EXTENSION)) {
+                        array_push($this->inHouseRepos, $result);
 
                         // Búsqueda de composers
                         $directoryIteration = new DirectoryIterator($this->getRepoDir() . "/" . $result);
                         if (!empty($directoryIteration)) {
                             foreach ($directoryIteration as $key => $composer_file) {
                                 if (pathinfo($composer_file, PATHINFO_EXTENSION) == 'json')
-                                    array_push($composerList, json_decode(file_get_contents($composer_file->getRealPath()), true));
+                                    array_push($this->composerList, json_decode(file_get_contents($composer_file->getRealPath()), true));
                             }
                         }
                     }
                 }
             }
 
-            if (!empty($composerList)) {
-                foreach ($composerList as $composer) {
-                    $flatElements[] = ["name" => $composer["name"], "child" => array_keys($composer["require"])];
-                }
-            }
+            if (!empty($this->composerList)) {
+                foreach ($this->composerList as $composer) {
 
-            foreach ($flatElements as $key => $flatElement) {
-                foreach ($flatElements as $toCompareFlat) {
-                    if (in_array($flatElement["name"], $toCompareFlat["child"]))
-                        $flatElements[$key]["parents"][] = $toCompareFlat["name"];
+                    $flatElements[] = ["name" => $composer["name"], "child" => $this->buildTree($composer)];
                 }
-            }
 
-            if (!empty($flatElements)) {
-                $arrayResponse = $flatElements;
+                if (!empty($flatElements)) {
+                    $arrayResponse = $flatElements;
+                }
             }
         }
 
 
         return $arrayResponse;
     }
+
+    public function buildTree($composer)
+    {
+
+        $branch = array();
+        foreach ($composer["require"] as $key => $child) {
+            $repoIndex = array_search($key, array_column($this->composerList, 'name'));
+            $branch[] = ["name" => $key, "child" => $this->buildTree($this->composerList[$repoIndex])];
+        }
+
+        return $branch;
+    }
 }
 
 $relacion = new Relacion();
-print_r($relacion->getPackageRelation());
+print_r(json_encode($relacion->getPackageRelation()));
